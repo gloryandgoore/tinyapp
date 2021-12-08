876d238321e3ccb77b61fc14e2703b28685364b0
Caseyann <goore_44@hotmail.ca>
2021-12-08 18:26:08 -0500

    redirecting

--- express_server.js
+++ express_server.js
@@ -1,33 +1,22 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080; //default port 8080
 
-function generateRandomString() {
-    let shortened = "";
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    for (let i = 0; i < 6; i++) {
-        shortened += characters.charAt(Math.floor(Math.random() * 6));
-    }
-    return shortened
-}
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-    "bZxVn2": "http://www.lighthouselabs.ca",
+    "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
-};
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+  };
 
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-});
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
@@ -51,9 +40,42 @@ app.get("/urls/:shortURL", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+});
+
+
 app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+    // const longURL = req.body.longURL;
+    // console.log(req.body)
+    let short = generateRandomString();
+    urlDatabase[short] = req.body.longURL;
+    res.redirect(`/urls/${short}`);
   });
 
- 
+  app.post("/urls/:shortUrl/delete", (req, res) => {
+      const shortURL = req.params.shortUrl;
+      delete urlDatabase[shortURL];
+      res.redirect("/urls")
+  });
+
+  app.post("/urls/:shortUrl", (req, res) => {
+      const longURL = req.body.longURL;
+      shortURL = req.params.shortUrl
+      urlDatabase[shortURL] = longURL;
+      res.redirect("/urls")
+  })
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+    let shortened = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    for (let i = 0; i < 6; i++) {
+        shortened += characters.charAt(Math.floor(Math.random() * 6));
+    }
+    return shortened
+}
